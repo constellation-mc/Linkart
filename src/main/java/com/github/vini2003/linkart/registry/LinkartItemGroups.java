67ca593ef6670ba09db9d055b391de367a91e77c
melontini <melontini@yandex.ru>
2022-05-11 16:52:29 +0700

    enhanced 1.16.5

--- src/main/java/com/github/vini2003/linkart/registry/LinkartItemGroups.java
+++ src/main/java/com/github/vini2003/linkart/registry/LinkartItemGroups.java
@@ -1,10 +1,6 @@
 package com.github.vini2003.linkart.registry;
 
-import com.github.vini2003.linkart.configuration.LinkartConfiguration;
-import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
 import net.minecraft.item.ItemGroup;
-import net.minecraft.item.ItemStack;
-import net.minecraft.util.Identifier;
 
 public class LinkartItemGroups {
    public static ItemGroup LINKART_GROUP;
