6de85e5d44e6b096e8371b1235a9a68bbe9056b5
melontini <melontini@yandex.ru>
2022-05-16 15:57:08 +0700

    Pain Now carts will unlink when their parent is out of child's pathfinding distance

--- src/main/java/com/github/vini2003/linkart/mixin/AbstractMinecartEntityMixin.java
+++ src/main/java/com/github/vini2003/linkart/mixin/AbstractMinecartEntityMixin.java
@@ -3,9 +3,18 @@ package com.github.vini2003.linkart.mixin;
 import java.util.UUID;
 
 import com.github.vini2003.linkart.accessor.AbstractMinecartEntityAccessor;
+import com.github.vini2003.linkart.configuration.LinkartConfiguration;
+import com.github.vini2003.linkart.registry.LinkartConfigurations;
+import com.github.vini2003.linkart.registry.LinkartNetworks;
 import com.github.vini2003.linkart.utility.CollisionUtils;
 import com.github.vini2003.linkart.utility.RailUtils;
 
+import net.fabricmc.fabric.api.network.ClientSidePacketRegistry;
+import net.minecraft.block.AbstractRailBlock;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.util.ItemScatterer;
 import org.apache.commons.lang3.mutable.MutableDouble;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
@@ -23,102 +32,124 @@ import net.minecraft.world.World;
 
 @Mixin({AbstractMinecartEntity.class})
 public abstract class AbstractMinecartEntityMixin implements AbstractMinecartEntityAccessor {
-   @Unique
-   UUID nextUuid;
-   @Unique
-   private AbstractMinecartEntity previous;
-   @Unique
-   private AbstractMinecartEntity next;
-   @Unique
-   private UUID previousUuid;
-   @Override
-   public AbstractMinecartEntity getPrevious() {
-      if (this.previous == null && this.getPreviousUuid() != null && !((AbstractMinecartEntity)(Object)this).world.isClient) {
-         this.previous = (AbstractMinecartEntity)((ServerWorld)((AbstractMinecartEntity)(Object)this).world).getEntity(this.getPreviousUuid());
-      }
+    @Unique
+    UUID nextUuid;
+    @Unique
+    private AbstractMinecartEntity previous;
+    @Unique
+    private AbstractMinecartEntity next;
+    @Unique
+    private UUID previousUuid;
 
-      return this.previous;
-   }
+    @Override
+    public AbstractMinecartEntity getPrevious() {
+        if (this.previous == null && this.getPreviousUuid() != null && !((AbstractMinecartEntity) (Object) this).world.isClient) {
+            this.previous = (AbstractMinecartEntity) ((ServerWorld) ((AbstractMinecartEntity) (Object) this).world).getEntity(this.getPreviousUuid());
+        }
 
-   @Override
-   public void setPrevious(AbstractMinecartEntity previous) {
-      this.previous = previous;
-      this.nextUuid = previous == null ? null : previous.getUuid();
-   }
+        return this.previous;
+    }
 
-   @Override
-   public AbstractMinecartEntity getNext() {
-      if (this.next == null && this.getNextUuid() != null && !((AbstractMinecartEntity)(Object)this).world.isClient) {
-         this.next = (AbstractMinecartEntity)((ServerWorld)((AbstractMinecartEntity)(Object)this).world).getEntity(this.getNextUuid());
-      }
+    @Override
+    public void setPrevious(AbstractMinecartEntity previous) {
+        this.previous = previous;
+        this.nextUuid = previous == null ? null : previous.getUuid();
+    }
 
-      return this.next;
-   }
+    @Override
+    public AbstractMinecartEntity getNext() {
+        if (this.next == null && this.getNextUuid() != null && !((AbstractMinecartEntity) (Object) this).world.isClient) {
+            this.next = (AbstractMinecartEntity) ((ServerWorld) ((AbstractMinecartEntity) (Object) this).world).getEntity(this.getNextUuid());
+        }
 
-   @Override
-   public void setNext(AbstractMinecartEntity next) {
-      this.next = next;
-      this.nextUuid = next == null ? null : next.getUuid();
-   }
+        return this.next;
+    }
 
-   @Override
-   public UUID getPreviousUuid() {
-      return this.previousUuid;
-   }
+    @Override
+    public void setNext(AbstractMinecartEntity next) {
+        this.next = next;
+        this.nextUuid = next == null ? null : next.getUuid();
+    }
 
-   @Override
-   public void setPreviousUuid(UUID uuid) {
-      this.previousUuid = uuid;
-   }
+    @Override
+    public UUID getPreviousUuid() {
+        return this.previousUuid;
+    }
 
-   @Override
-   public UUID getNextUuid() {
-      return this.nextUuid;
-   }
+    @Override
+    public void setPreviousUuid(UUID uuid) {
+        this.previousUuid = uuid;
+    }
 
-   @Override
-   public void setNextUuid(UUID uuid) {
-      this.nextUuid = uuid;
-   }
+    @Override
+    public UUID getNextUuid() {
+        return this.nextUuid;
+    }
 
-   @Inject(
-      at = {@At("HEAD")},
-      method = {"tick()V"}
-   )
-   void onTickCommon(CallbackInfo callbackInformation) {
-      World mixedWorld = ((AbstractMinecartEntity)(Object)this).world;
-      AbstractMinecartEntity next = (AbstractMinecartEntity)(Object)this;
+    @Override
+    public void setNextUuid(UUID uuid) {
+        this.nextUuid = uuid;
+    }
 
-      AbstractMinecartEntityAccessor accessor = (AbstractMinecartEntityAccessor)next;
-      if (!mixedWorld.isClient && accessor.getPrevious() != null) {
-         AbstractMinecartEntity previous = accessor.getPrevious();
-         Pair<BlockPos, MutableDouble> nextRail = RailUtils.getNextRail(next, previous);
-         Vec3d nextVelocity = RailUtils.getNextVelocity(next, previous);
-         if (nextVelocity != null) {
-            if (nextRail == null) {
-               next.setVelocity(0, 0, 0);
+    @Inject(
+            at = {@At("HEAD")},
+            method = {"tick()V"}
+    )
+    void onTickCommon(CallbackInfo callbackInformation) {
+        World mixedWorld = ((AbstractMinecartEntity) (Object) this).world;
+        AbstractMinecartEntity next = (AbstractMinecartEntity) (Object) this;
+
+        AbstractMinecartEntityAccessor accessor = (AbstractMinecartEntityAccessor) next;
+        if (!mixedWorld.isClient && accessor.getPrevious() != null) {
+            AbstractMinecartEntity previous = accessor.getPrevious();
+            Pair<BlockPos, MutableDouble> nextRail = RailUtils.getNextRail(next, previous);
+            Vec3d nextVelocity = RailUtils.getNextVelocity(next, previous);
+            if (nextVelocity != null) {
+                if ((next.getPos().distanceTo(previous.getPos()) > LinkartConfigurations.INSTANCE.getConfig().getPathfindingDistance()) && getPrevious() != null) {
+                    unlinkCarts();
+                } else if (previous.getVelocity().getY() == 0 && previous.getVelocity().getX() == 0 && previous.getVelocity().getZ() == 0) {
+                    next.setVelocity(0, 0, 0);
+                } else if (!(next.world.getBlockState(next.getBlockPos()).getBlock() instanceof AbstractRailBlock)) {
+                    next.setVelocity(0, 0, 0);
+                } else {
+                    next.setVelocity(nextVelocity);
+                }
             }
-            else if (previous.getVelocity().getX() == 0 && previous.getVelocity().getZ() == 0) {
-               next.setVelocity(0, 0, 0);
-            }
-            //else if (previous.getPos().distanceTo(next.getPos()) > 1 && nextRail != null && (previous.getVelocity().getX() != 0 || previous.getVelocity().getZ() != 0)) {
-            //  next.setVelocity(nextVelocity.getX() + 1, nextVelocity.getY() + 1, nextVelocity.getZ() + 1);
-            //}
-            else {next.setVelocity(nextVelocity);}
-         }
-      }
+        }
+    }
 
-   }
+    private void unlinkCarts() {
+        AbstractMinecartEntity next = (AbstractMinecartEntity) (Object) this;
+        AbstractMinecartEntityAccessor accessor = (AbstractMinecartEntityAccessor) next;
+        AbstractMinecartEntity previous = accessor.getPrevious();
+        UUID nextUUID = next.getUuid();
+        UUID previousUUID = previous.getUuid();
+        ServerWorld serverWorld = (ServerWorld) ((AbstractMinecartEntity) (Object) this).world;
+        AbstractMinecartEntity entityA = (AbstractMinecartEntity) serverWorld
+                .getEntity(nextUUID);
+        AbstractMinecartEntity entityB = (AbstractMinecartEntity) serverWorld
+                .getEntity(previousUUID);
+        assert entityA != null;
+        ((AbstractMinecartEntityAccessor) previous).setNext(null);
+        assert entityB != null;
+        ((AbstractMinecartEntityAccessor) next).setPrevious(null);
+        ItemScatterer.spawn(
+                entityA.world,
+                entityA.getX(),
+                entityA.getY(),
+                entityA.getZ(),
+                new ItemStack(Items.CHAIN));
+    }
 
-   @Inject(
-      at = {@At("HEAD")},
-      method = {"pushAwayFrom(Lnet/minecraft/entity/Entity;)V"},
-      cancellable = true
-   )
-   void onPushAway(Entity entity, CallbackInfo callbackInformation) {
-      if (!CollisionUtils.shouldCollide((Entity)(Object)this, entity)) {
-         callbackInformation.cancel();
-      }
+    @Inject(
+            at = {@At("HEAD")},
+            method = {"pushAwayFrom(Lnet/minecraft/entity/Entity;)V"},
+            cancellable = true
+    )
+    void onPushAway(Entity entity, CallbackInfo callbackInformation) {
+        if (!CollisionUtils.shouldCollide((Entity) (Object) this, entity)) {
+            callbackInformation.cancel();
+        }
 
-   }
+    }
 }
