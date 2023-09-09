7ecf74fbf60ab9248d44b5b02f0daeacbb65cc6f
George Zhang <geetransit@gmail.com>
2023-09-09 18:54:29 -0400

    Fix velocityMultiplier not being applied

--- src/main/java/com/github/vini2003/linkart/mixin/AbstractMinecartEntityMixin.java
+++ src/main/java/com/github/vini2003/linkart/mixin/AbstractMinecartEntityMixin.java
@@ -65,7 +65,7 @@ public abstract class AbstractMinecartEntityMixin extends Entity implements Link
                 Vec3d pos2 = linkart$getFollowing().getPos();
                 double dist = Math.abs(pos.distanceTo(pos2)) - 1.2;
                 Vec3d vec3d = pos.relativize(pos2);
-                vec3d.multiply(Linkart.CONFIG.velocityMultiplier);
+                vec3d = vec3d.multiply(Linkart.CONFIG.velocityMultiplier);
 
                 if (dist <= 1) {
                     setVelocity(vec3d.multiply(dist * 0.3));
